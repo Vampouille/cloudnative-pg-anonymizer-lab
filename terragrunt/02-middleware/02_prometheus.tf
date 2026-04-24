35d09b6af492d16e6ae41490fd25a5037f807266
Julien Acroute <julien.acroute@camptocamp.com>
2026-04-24 12:27:03 +0200

    feat: deploy monitoring

---
+++ terragrunt/02-middleware/02_prometheus.tf
@@ -0,0 +1,19 @@
+resource "helm_release" "kube_prometheus_stack" {
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "70.7.0"
+
+  namespace = "kube-prometheus-stack"
+  #disable_webhooks   = true
+  #disable_crd_hooks = true
+  #skip_crds         = true
+  dependency_update = true
+  create_namespace = true
+  #timeout           = 10800
+  atomic          = true
+  upgrade_install = true
+  force_update    = true
+
+  values = [file("${path.module}/values-kube-prometheus-stack.yaml")]
+}
